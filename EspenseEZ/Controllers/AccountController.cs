d30238215ebb8c17cd59ea915dced8609fee8e48
Louise Micko Tabarno <l.tabarno.549810@umindanao.edu.ph>
2026-05-02 13:33:15 +0800

    initial commit

--- EspenseEZ/Controllers/AccountController.cs
+++ EspenseEZ/Controllers/AccountController.cs
@@ -4,8 +4,36 @@ namespace ExpenseEZ.Controllers
 {
 	public class AccountController : Controller
 	{
+		[HttpGet]
 		public IActionResult Login()
 		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Login(string username, string password)
+		{
+			if (username == "admin" && password == "admin123")
+			{
+				return RedirectToAction("Dashboard", "Admin");
+			}
+
+			if (username == "employee" && password == "employee123")
+			{
+				return RedirectToAction("Dashboard", "Employee");
+			}
+
+			if (username == "manager" && password == "manager123")
+			{
+				return RedirectToAction("Dashboard", "Manager");
+			}
+
+			if (username == "finance" && password == "finance123")
+			{
+				return RedirectToAction("Dashboard", "Finance");
+			}
+
+			ViewBag.Error = "Invalid username or password.";
 			return View();
 		}
 	}
