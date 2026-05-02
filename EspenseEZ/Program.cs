6b636a932c5e89042a04e86b69e049bb319832e8
ltabarno549810-coder <l.tabarno.549810@umindanao.edu.ph>
2026-05-02 17:04:36 +0800

    Change database provider from SQL Server to Npgsql

--- EspenseEZ/Program.cs
+++ EspenseEZ/Program.cs
@@ -1,31 +1,30 @@
-using ExpenseEZ.Data;
+using EspenseEZ.Data;
 using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-// Add services to the container.
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Account}/{action=Login}/{id?}")
-	.WithStaticAssets();
+    name: "default",
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
